e41e8a0091a0d7b90b93d93096956633faa55e4d
NyBeata <78854516+NyBeata@users.noreply.github.com>
2021-04-07 13:08:42 +0200

    Add files via upload

---
+++ I40DFC_0407/i40fdc_named.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main()
+{
+    int child;
+
+    mkfifo("i40fdc", S_IRUSR | S_IWUSR);
+
+    child = fork();
+
+    if(child > 0){
+        char s[1024];
+        int fd;
+
+        fd = open("i40fdc", O_RDONLY);
+        read(fd, s, sizeof(s));
+        printf("%s", s);
+
+        close(fd);
+        unlink("i40fdc");
+    } else if(child == 0){
+        int fd;
+
+        fd = open("i40fdc", O_WRONLY);
+        write(fd, "Nyiri Beata\n", 13);
+        close(fd);
+    }
+}
